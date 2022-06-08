031b79b4fb25c45bccee87c5ddf98b21cfc08791
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-06-09 00:23:33 +0530

    init

---
+++ init.js
@@ -0,0 +1,24 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+//  Import data from config.json
+const { prefix, token } = require('./config.json');
+// Create a new client instance
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ] 
+});
+// slash command prereq
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
