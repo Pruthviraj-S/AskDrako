200c22b99bab8794cd33c6779bf665469bf4cd92
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-06-23 00:46:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
 // use promisify to convert callback into promise using utils, add table, glob
